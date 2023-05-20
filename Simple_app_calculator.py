ae1e28fd999c841444dd3665a2ea3cd9759e0d6e
CateMorx <morco.catelynjoy.12192003@gmail.com>
2023-05-21 03:56:23 +0800

    fixed def for the 4 operations

--- Simple_app_calculator.py
+++ Simple_app_calculator.py
@@ -26,6 +26,10 @@ class calculator():
                 raise TypeError
             elif not num_1:
                 raise ValueError
+            elif not isinstance(num_2, int):
+                raise TypeError
+            elif not num_2:
+                raise ValueError
         except TypeError:
             messagebox.showerror("TypeError", "Number 1 must be an integer")
         except ValueError:
@@ -35,27 +39,61 @@ class calculator():
             self.result=result
             return
     # def for performing the subtraction operation 
-    elif operation == 2:
-        result = num1 - num2
-        result_label.config(text="Result: " + str(result))
-    # def for performing the multiplication operation 
-    elif operation == 3:
-        result = num1 * num2
-        result_label.config(text="Result: " + str(result))
-    # def for performing the division operation 
-    elif operation == 4:
+    def operation_subtraction(self,num_1,num_2):
         try:
-            result = num1 / num2
-            result_label.config(text="Result: " + str(result))
-        #Does not allow zero division
-        except ZeroDivisionError:
-            num2_entry.delete(0, tkinter.END)
-            return messagebox.showerror("Error on Second Number Entry", "Invalid Input, Cannot Divide by Zero")
-# Asking user if they want to try again or not
-    choice = messagebox.askyesno("Try again?", "Do you want to try again?")
-    #Creates a pop-up Thank you message and closes the program
-    if not choice:
-        messagebox.showinfo("Thank you message", "Thank you!")
-        window.destroy()
-    #Creates def for Input of num1 & num2
+            if not isinstance(num_1, int):
+                raise TypeError
+            elif not num_1:
+                raise ValueError
+            elif not isinstance(num_2, int):
+                raise TypeError
+            elif not num_2:
+                raise ValueError
+        except TypeError:
+            messagebox.showerror("TypeError", "Number 1 must be an integer")
+        except ValueError:
+            messagebox.showerror("ValueError", "Number 1 must be an integer")
+        else:
+            result = self.num_1 - self.num_2
+            self.result=result
+            return
+    # def for performing the multiplication operation 
+    def operation_multiplication(self,num_1,num_2):
+        try:
+            if not isinstance(num_1, int):
+                raise TypeError
+            elif not num_1:
+                raise ValueError
+            elif not isinstance(num_2, int):
+                raise TypeError
+            elif not num_2:
+                raise ValueError
+        except TypeError:
+            messagebox.showerror("TypeError", "Number 1 must be an integer")
+        except ValueError:
+            messagebox.showerror("ValueError", "Number 1 must be an integer")
+        else:
+            result = self.num_1 * self.num_2
+            self.result=result
+            return
+    # def for performing the division operation 
+    def operation_division(self,num_1,num_2):
+        try:
+            if not isinstance(num_1, int):
+                raise TypeError
+            elif not num_1:
+                raise ValueError
+            elif not isinstance(num_2, int):
+                raise TypeError
+            elif not num_2:
+                raise ValueError
+        except TypeError:
+            messagebox.showerror("TypeError", "Number 1 must be an integer")
+        except ValueError:
+            messagebox.showerror("ValueError", "Number 1 must be an integer")
+        else:
+            result = self.num_1/ self.num_2
+            self.result=result
+            return
+
 
