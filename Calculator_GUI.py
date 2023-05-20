9a13e4bbba6d893833da19b69a99a8b1f557ba58
CateMorx <morco.catelynjoy.12192003@gmail.com>
2023-05-21 04:13:19 +0800

    imports necessary elements, creates class for GUI

--- Calculator_GUI.py
+++ Calculator_GUI.py
@@ -3,8 +3,16 @@
 #Redo calculator program applying the OOP concept previously discussed
 
 #Imports necessary elements
+import tkinter as tkinter
+from tkinter import messagebox
+from Simple_app_calculator import calculator
 #Creates class for calculator GUI
+calc = calculator()
+class calculator_GUI:
     #Creates def for Parametrized Constructor and GUI
+     def __init__(self, calc, GUI):
+        self.GUI = GUI
+        self.calc= calc
     #Def for button functions
     #Def fo asking user if they want to try again or not
 #starts the event loop of the GUI application
