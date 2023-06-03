aeb8867260e8cf0097d71f22b5d62214dfa00ccf
CateMorx <morco.catelynjoy.12192003@gmail.com>
2023-06-03 18:34:28 +0800

    creates file for child class

---
+++ Calculator_Inheritance.py
@@ -0,0 +1,14 @@
+#Created by: Catelyn Joy M.Morco from BSCPE 1-4
+#Assignment 8
+#Create a new class that inherits from existing class in your calculator program - Add new method(s) or override an existing method(s) in the new class
+# - Use the new class and the new method in your calculator program
+
+#Imports necessary elements
+import tkinter as tkinter
+from tkinter import messagebox
+import math
+from Simple_app_calculator import calculator
+
+#creates child class
+#Adds new power method
+#adds new square root method
